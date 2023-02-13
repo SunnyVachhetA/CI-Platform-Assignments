1a1a4360e06dc0012108abb167d621bbfbefebb9
SunnyVachhetA <sunnyvachheta89@gmail.com>
2023-02-13 10:59:44 +0530

    Forgot password page added

--- MVC/CI-Platform/CIPlatformWeb/Controllers/HomeController.cs
+++ MVC/CI-Platform/CIPlatformWeb/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace CIPlatformWeb.Controllers
             return View();
         }
 
+        public IActionResult ForgotPassword()
+        {
+            Console.WriteLine("Forgot Password called");
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
