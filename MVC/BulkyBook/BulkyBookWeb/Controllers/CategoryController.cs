f35fac05c6009f5c52a00034d5fdf3013dba8411
SunnyVachhetA <sunnyvachheta89@gmail.com>
2023-02-13 18:07:11 +0530

    Category CRUD complete

---
+++ MVC/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,93 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers;
+public class CategoryController : Controller
+{
+    private readonly AppDbContext _db;
+    public CategoryController(AppDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        //var categoryList = _db.Categories.ToList(); 
+        IEnumerable<Category> categoryList = _db.Categories;
+        return View( categoryList );
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if(ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();  
+            TempData["success"] = "Category Added Successfully";
+            return RedirectToAction("Index");
+        }
+        TempData["failed"] = "Category Creation Failed!";  
+        return View(obj);
+    }    
+
+
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0)
+            return NotFound();
+        
+        Category? obj = _db.Categories.Find(id);
+
+        return (obj == null) ? NotFound() : View(obj);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if(ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();  
+            TempData["success"] = "Category Updated Successfully";
+            return RedirectToAction("Index");
+        }
+        TempData["failed"] = "Category Updatation Failed!";  
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            TempData["failed"] = "Category Not Found!";  
+            return NotFound();
+        }
+        Category? obj = _db.Categories.Find(id);
+
+        return (obj == null) ? NotFound() : View(obj);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST( int? id )
+    {
+        Category? obj = _db.Categories.Find(id);
+        if(obj == null) { 
+            TempData["failed"] = "Category Deletion Failed!";  
+            return NotFound(); 
+        }
+
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category Deleted Successfully";
+        return RedirectToAction("Index");    
+    }
+}
---
+++ MVC/BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace BulkyBookWeb.Models;
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name cannot be empty!")]
+    public string Name { get; set; }
+
+    [Range(1, 100, ErrorMessage = "Must be between 1 to 100!")]
+    [DisplayName("Display Order")] public int DisplayOrder { get; set; }
+
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+}
